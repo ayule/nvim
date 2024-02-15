9ef6974e820652110487ec188ce4a17d72a173ad
ayule <68344743+ayule@users.noreply.github.com>
2024-02-15 13:31:44 -0700

    Add plugins/treesitter.lua
    Secondary changes:
    - lazyvim.json from enabling various LazyExtras
    - Corresponding lazy-lock.json changes

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "json",
+        "lua",
+        "python",
+        "query",
+        "regex",
+        "vim",
+        "yaml",
+        "go",
+        "terraform",
+      },
+    },
+  },
+}
