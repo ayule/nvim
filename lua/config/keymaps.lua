ac58b4816bce5fcf1054d47140d1bab496f9dbce
ayule <68344743+ayule@users.noreply.github.com>
2024-02-16 10:46:01 -0700

    Adjust clipboard and yank behaviors
    - No longer yank by default to system clipboard
    - Add keymaps to allow yanking to system clipboard
    - Enable yanky plugin

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,5 +16,5 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "Q", "<nop>")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+-- vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
