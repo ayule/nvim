117e33f9d7ad9487600f377c0d4a11ee45c72986
ayule <68344743+ayule@users.noreply.github.com>
2024-02-16 18:13:36 -0700

    Change colorscheme to rose-pine

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,14 @@
 return {
   { "ellisonleao/gruvbox.nvim" },
   { "nyoom-engineering/oxocarbon.nvim" },
+  { "rose-pine/neovim", name = "rose-pine" },
+  { "rebelot/kanagawa.nvim" },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "oxocarbon",
+      colorscheme = "rose-pine",
     },
   },
 }
