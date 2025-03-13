f6baa94cb4ae356bce45494100f2c27a5ef8928e
ayule <68344743+ayule@users.noreply.github.com>
2025-03-13 10:21:13 -0600

    Add descriptions to custom keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,18 +2,23 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Bring up next line" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Page down" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Page up" })
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result" })
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to + register" })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank line to + register" })
 
 -- vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "Q", "<nop>")
 
-vim.keymap.set("n", "<leader>rs", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set(
+  "n",
+  "<leader>rs",
+  [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+  { desc = "sed-based search and replace" }
+)
